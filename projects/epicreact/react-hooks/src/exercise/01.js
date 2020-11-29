e35ce8afdd78d456681848da221f32bea863b2d2
Yaseen Kadir <yaseenkadir@users.noreply.github.com>
2020-11-29 17:46:12 +1100

    React Hooks - Exercise 01

--- projects/epicreact/react-hooks/src/exercise/01.js
+++ projects/epicreact/react-hooks/src/exercise/01.js
@@ -3,19 +3,18 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
-
+// I should go with the object syntax more often, and handle empty values better
+// The solution
+function Greeting(props) {
+  const [name, setName] = React.useState(props.initialName)
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
-
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +22,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="friend"/>
 }
 
 export default App
